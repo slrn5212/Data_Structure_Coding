d596b62399d088abc3578402e06240f8b9cefb8b
Yaohui Huang <hyh5212@163.com>
2019-08-16 12:45:08 +0800

    Update QuickSort Algorithm.

--- Data_Structure_Coding/SortMethod.h
+++ Data_Structure_Coding/SortMethod.h
@@ -306,6 +306,118 @@ void OptimizemergeSortBottonToUp(T arr[], int n) {
 }
 
 
+/*
+	Quick Sort
+*/
+template <typename T>
+int __partition2(T arr[], int l, int r) {
+	swap(arr[l], arr[rand() % (r - l + 1) + l]);  // Add this process to randomly choose demarcating element.
+	T v = arr[l];
+
+	//arr[l+i ... j] < v; arr[j+1 ... i] > v
+	int i = l + 1, j = r;
+	while (true) {
+		//From front to behind to find a even bigger number.
+		//From behind to front to find a even smaller number.
+		while (i <= r&& arr[i] < v) i++;
+		while (j >= l + 1 && arr[j] > v) j--;
+		if (i > j) break;
+		swap(arr[i], arr[j]);
+		i++;
+		j--;
+	}
+
+	swap(arr[l], arr[j]);
+
+	return j;
+}
+
+template <typename T>
+int __partition(T arr[], int l, int r) {
+
+	swap(arr[l], arr[rand() % (r - l + 1) + l]);  // Add this process to randomly choose demarcating element.
+	T v = arr[l];
+	
+	//arr[l+i ... j] < v;arr[j+1 ... i] > v
+	int j = l;
+	for (int i = l + 1;i <= r;i++) {
+		if (arr[i] < v) {
+			swap(arr[j + 1], arr[i]);
+			j++;
+		}
+	}
+
+	swap(arr[l], arr[j]);
+	return j;
+}
+
+// sort the range of [l ... r]
+template <typename T>
+void __quickSort(T arr[], int l, int r) {
+	//if (l >= r) return;
+	if (r - l <= 15) {
+		OptimizedInsertionSort(arr, l, r);
+		return;
+	}
+	int p = __partition(arr, l, r);
+	__quickSort(arr, l, p - 1);
+	__quickSort(arr, p + 1, r);
+}
+
+template <typename T>
+void __quickSort2(T arr[], int l, int r) {
+	//if (l >= r) return;
+	if (r - l <= 15) {
+		OptimizedInsertionSort(arr, l, r);
+		return;
+	}
+	int p = __partition2(arr, l, r);
+	__quickSort2(arr, l, p - 1);
+	__quickSort2(arr, p + 1, r);
+}
+
+template <typename T>
+void __quickSort3(T arr[], int l, int r) {
+	//if (l >= r) return;
+	if (r - l <= 15) {
+		OptimizedInsertionSort(arr, l, r);
+		return;
+	}
+
+	// partition
+	swap(arr[l], arr[rand() % (r - l + 1) + l]);
+	T v = arr[l];
+
+	int lt = l;		//arr[l+1 ... lt] < v
+	int gt = r + 1; //arr[gt ... r] > v
+	int i = l + 1;	//arr[lt+1 ... i] == v
+	while (i < gt) {
+		if (arr[i] < v) {
+			swap(arr[i], arr[lt + 1]);
+			lt++;
+			i++;
+		}
+		else if(arr[i] > v) {
+			swap(arr[i], arr[gt - 1]);
+			gt--;
+		}
+		else {// arr[i] == v
+			i++;
+		}
+	}
+
+	swap(arr[l], arr[lt]);
+
+	__quickSort3(arr, l, lt - 1);
+	__quickSort3(arr, gt, r);
+}
+
+template <typename T>
+void quickSort(T arr[], int n) {
+	srand(time(NULL));		// The partial of randomly select.
+	__quickSort3(arr, 0, n - 1);
+}
+
 
 
 #endif // !SORTHEADER_SELECT_H
--- Data_Structure_Coding/main.cpp
+++ Data_Structure_Coding/main.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 
 int main() {
-	int n = 50000;
-	int* arr = SortTestHelper::generateRandomArray(n, 0, n);
+	int n = 100000;
+	int* arr = SortTestHelper::generateRandomArray(n, 0, 10);
 	int *arr2 = SortTestHelper::copyIntArray(arr, n);
 
-	SortTestHelper::testSort("Binary Insertion Sort", OptimizemergeSortBottonToUp, arr, n);
+	SortTestHelper::testSort("Binary Insertion Sort", quickSort, arr, n);
 	SortTestHelper::testSort("Merge Sort", mergeSort, arr2, n);
 
 	delete[] arr;
