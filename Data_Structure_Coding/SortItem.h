e0b5f4b51a7e2a8a6f9b789500c100cb00b9a0e9
Yaohui Huang <hyh5212@163.com>
2019-08-20 11:46:18 +0800

    Steal.

--- Data_Structure_Coding/SortItem.h
+++ Data_Structure_Coding/SortItem.h
@@ -201,6 +201,93 @@ public:
 	}
 };
 
-
+//template<typename Item>
+//class IndexMaxHeap {
+//
+//private:
+//	Item* data;
+//	int* indexes;
+//	int count;
+//	int capacity;
+//
+//	void shiftUp(int k) {
+//		while (k > 1 && data[k / 2] < data[k])
+//		{
+//			swap(data[k / 2], data[k]);
+//			k /= 2;
+//		}
+//	}
+//
+//	void shiftDown(int k) {
+//		while (2 * k <= count) {
+//			int j = 2 * k; // 在此轮循环中,data[k]和data[j]交换位置
+//			if (j + 1 <= count && data[j + 1] > data[j])
+//				j++;
+//			// data[j] 是 data[2*k]和data[2*k+1]中的最大值
+//
+//			if (data[k] >= data[j])
+//				break;
+//
+//			swap(data[k], data[j]);
+//			k = j;
+//		}
+//	}
+//
+//
+//public:
+//	//Original Process  O(nlogn)
+//	MaxHeap(int capacity) {
+//		data = new Item[capacity + 1];	//Index begin in the '1' place.
+//		indexes = new int[capacity + 1];
+//		count = 0;
+//		this->capacity = capacity;
+//	}
+//
+//	//Heapify Process   O(n)
+//	MaxHeap(Item arr[], int n) {
+//
+//		data = new Item[n + 1];
+//		capacity = n;
+//		for (int i = 0;i < n;i++) {
+//			data[i + 1] = arr[i];
+//		}
+//		count = n;
+//		for (int i = count / 2;i >= 1;i--)
+//			shiftDown(i);
+//	}
+//
+//	~MaxHeap() {
+//		delete[] data;
+//		delete[] indexes;
+//	}
+//
+//	int size() {
+//		return count;
+//	}
+//
+//	bool isEmpty() {
+//		return count == 0;
+//	}
+//
+//	// Insert - ShiftUp
+//	void insert(int i, Item item) {
+//		assert(count + 1 <= capacity);
+//		data[count + 1] = item;
+//		count++;
+//		shiftUp(count);
+//	}
+//
+//	// Return Max Number - ShiftDown
+//	Item extractMax() {
+//		assert(count > 0);
+//		Item ret = data[1];
+//		swap(data[1], data[count]);
+//		count--;
+//		shiftDown(1);
+//
+//		return ret;
+//	}
+//
+//};
 
 #endif // !SORTITEM_H
